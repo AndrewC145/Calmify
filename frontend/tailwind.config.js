d51d4410abd5a5b8a17dec90c85d2c583190f2a9
Andrew Cao <andrewcao618@gmail.com>
2025-02-06 00:16:32 -0500

    Created Home Page for XL Devices

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/pages/.{html,js}"],
+  theme: {
+    screens: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

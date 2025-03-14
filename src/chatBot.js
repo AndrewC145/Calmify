fb4901087e86fc63f7ddb83f90fc4980028e967c
Andrew Cao <andrewcao618@gmail.com>
2025-03-13 22:49:20 -0400

    Added User Messages

--- src/chatBot.js
+++ src/chatBot.js
@@ -1,7 +1,10 @@
 import ollama from 'ollama/browser';
 
+
 const sendBtn = document.querySelector(".send-msg");
 const question = document.querySelector(".chat-input");
+const chatIntro = document.querySelector(".chat-intro");
+const messageContainer = document.querySelector(".messages-container");
 
 async function getResponse(question) {
   const response = await ollama.chat({
@@ -16,21 +19,41 @@ async function getResponse(question) {
         content: question,
       },
     ],
-    stream: true,
   })
 
-  let fullMessage = '';
-
-  for await (const part of response) {
-    fullMessage += part.message.content;
-    console.log(fullMessage);
-  }
+  let fullMessage = response.message.content;
+  console.log(fullMessage);
+  displayBotMessage(fullMessage)
 }
 
 function sendMessage() {
+  chatIntro.remove();
+  messageContainer.classList.replace("items-center", "justify-between");
   const questionInput = question.value;
-
   getResponse(questionInput);
+  displayUserMessage();
+  question.value = "";
+}
+
+function displayUserMessage() {
+  const userMessage = document.createElement("div");
+  const userText = document.createElement("p");
+  userText.textContent = question.value;
+  userText.classList.add("user-text");
+  userMessage.classList.add("user-message");
+  userMessage.appendChild(userText);
+  messageContainer.appendChild(userMessage);
+}
+
+function displayBotMessage(message) {
+  const botMessage = document.createElement("div");
+  const botText = document.createElement("p");
+  botText.textContent = message;
+  botText.classList.add("bot-text");
+  botMessage.classList.add("bot-message");
+
+  botMessage.appendChild(botText);
+  messageContainer.appendChild(botMessage);
 }
 
 sendBtn.addEventListener("click", sendMessage);
@@ -38,10 +61,8 @@ question.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
     sendMessage();
-    question.value = "";
   }
 })
-
 
 export function initialize() {
 }
